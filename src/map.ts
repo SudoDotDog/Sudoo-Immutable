fe8ea2742eb41566c1e8371d74a6f9de4f87290f
WMXPY <wm@mengw.io>
2019-09-01 12:58:39 -0500

    init map

---
+++ src/map.ts
@@ -0,0 +1,25 @@
+/**
+ * @author WMXPY
+ * @namespace Immutable
+ * @description Map
+ */
+
+export class FrozenMap<K extends string, V extends any> {
+
+    public static from<K extends string, V extends any>(map: Record<K, V>) {
+
+        return new FrozenMap<K, V>(map);
+    }
+
+    private readonly _original: Record<K, V>;
+
+    private constructor(map: Record<K, V>) {
+
+        this._original = map;
+    }
+
+    public get original(): Record<K, V> {
+
+        return this._original;
+    }
+}
