3cab47dd0532bcde8326a2ca5f69acc78ff8b6a4
WMXPY <wm@mengw.io>
2019-09-01 12:59:57 -0500

    add list

---
+++ src/list.ts
@@ -0,0 +1,25 @@
+/**
+ * @author WMXPY
+ * @namespace Immutable
+ * @description List
+ */
+
+export class FrozenList<E extends any> {
+
+    public static from<E extends any>(list: E[]) {
+
+        return new FrozenList<E>(list);
+    }
+
+    private readonly _original: E[];
+
+    private constructor(list: E[]) {
+
+        this._original = list;
+    }
+
+    public get original(): E[] {
+
+        return this._original;
+    }
+}
