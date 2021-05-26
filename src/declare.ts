d196b92365839b01815d51dcd8266fb969b98dc4
WMXPY <wm@mengw.io>
2021-05-25 20:29:01 -0500

    add weiteable declare

--- src/declare.ts
+++ src/declare.ts
@@ -4,8 +4,17 @@
  * @description Declare
  */
 
-export type DraftFunction<T> = ((draft: T) => void) | ((draft: T) => T);
-export type AsyncDraftFunction<T> = DraftFunction<T> | ((draft: T) => Promise<void>) | ((draft: T) => Promise<T>);
+export type Writeable<T> = {
+
+    -readonly [P in keyof T]: T[P];
+};
+
+export type DeepWriteable<T> = {
+    -readonly [P in keyof T]: DeepWriteable<T[P]>;
+};
+
+export type DraftFunction<T> = ((draft: DeepWriteable<T>) => void) | ((draft: DeepWriteable<T>) => T);
+export type AsyncDraftFunction<T> = DraftFunction<T> | ((draft: DeepWriteable<T>) => Promise<void>) | ((draft: DeepWriteable<T>) => Promise<T>);
 
 export type ObjectEntry<K extends string, V extends any> = {
 
